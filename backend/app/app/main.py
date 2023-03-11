f9b462eb0c707d04f0203b25cdee9fc7c1434b7e
luovkle <roberto.silva.dev@protonmail.com>
2023-03-11 16:14:42 -0600

    cors is configured

--- backend/app/app/main.py
+++ backend/app/app/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.api_v1.api import api_router
 
 app = FastAPI(title="resuilder", version="0.0.1", docs_url="/", redoc_url=None)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:3000", "http://localhost:3000"],
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+)
 app.include_router(api_router)
