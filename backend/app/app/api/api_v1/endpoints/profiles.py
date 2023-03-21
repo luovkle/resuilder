3785f9b1a09be2a970182b565d05d3248f81ee86
luovkle <roberto.silva.dev@protonmail.com>
2023-03-20 20:19:57 -0600

    post and delete methods are removed from endpoints /profile

--- backend/app/app/api/api_v1/endpoints/profiles.py
+++ backend/app/app/api/api_v1/endpoints/profiles.py
@@ -3,14 +3,8 @@ from fastapi import APIRouter, Depends
 from app.api.deps import verify_token
 from app.schemas.payload import Payload
 from app.schemas.profile import ProfileRead
-from app.schemas.message import Message
 
 router = APIRouter(prefix="/profiles", tags=["profiles"])
-
-
-@router.post("", response_model=ProfileRead)
-def create_profile(token: Payload = Depends(verify_token)):
-    ...
 
 
 @router.get("/@me", response_model=ProfileRead)
@@ -20,9 +14,4 @@ def read_current_profile(token: Payload = Depends(verify_token)):
 
 @router.put("/@me", response_model=ProfileRead)
 def update_current_profile(token: Payload = Depends(verify_token)):
-    ...
-
-
-@router.delete("/@me", response_model=Message)
-def delete_current_profile(token: Payload = Depends(verify_token)):
     ...
