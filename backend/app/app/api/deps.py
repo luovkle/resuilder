b8523d915cfc7d322992244af693ed722cbe1d15
luovkle <roberto.silva.dev@protonmail.com>
2023-03-20 22:21:56 -0600

    added dependency to obtain access token

--- backend/app/app/api/deps.py
+++ backend/app/app/api/deps.py
@@ -1,5 +1,5 @@
 import requests
-from fastapi import Depends, HTTPException, status
+from fastapi import Depends, HTTPException, status, Request
 from fastapi.security import HTTPBearer
 from fastapi.security.http import HTTPAuthorizationCredentials
 
@@ -24,3 +24,7 @@ def verify_token(token: HTTPAuthorizationCredentials = Depends(token_auth_scheme
             status_code=status.HTTP_400_BAD_REQUEST, detail=result.get("msg", "")
         )
     return result
+
+
+def get_access_token(request: Request):
+    return request.headers["Authorization"].split()[1]
