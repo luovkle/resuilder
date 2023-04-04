5f095b4a25d920f7814951589267cf1e1cc23477
luovkle <roberto.silva.dev@protonmail.com>
2023-04-04 15:54:49 -0600

    ♻️ get_db dependency is implemented in endpoints /positions

--- backend/app/app/api/api_v1/endpoints/positions.py
+++ backend/app/app/api/api_v1/endpoints/positions.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Path, Depends, UploadFile
+from pymongo.database import Database
 
-from app.api.deps import verify_token
+from app.api.deps import verify_token, get_db
 from app.schemas.payload import Payload
 from app.schemas.position import PositionCreate, PositionRead, PositionUpdate
 from app.schemas.message import Message
@@ -11,45 +12,56 @@ router = APIRouter(prefix="/profiles", tags=["positions"])
 
 @router.post("/@me/positions", response_model=PositionRead)
 def create_position(
-    token: Payload = Depends(verify_token), *, position: PositionCreate
+    token: Payload = Depends(verify_token),
+    db: Database = Depends(get_db),
+    *,
+    position: PositionCreate,
 ):
-    return crud_position.create(token["sub"], position)
+    return crud_position.create(db, token["sub"], position)
 
 
 @router.get("/@me/positions", response_model=list[PositionRead])
-def read_current_positions(token: Payload = Depends(verify_token)):
-    return crud_position.read_many(token["sub"])
+def read_current_positions(
+    token: Payload = Depends(verify_token), db: Database = Depends(get_db)
+):
+    return crud_position.read_many(db, token["sub"])
 
 
 @router.get("/@me/positions/{id}", response_model=PositionRead)
 def read_current_position(
-    token: Payload = Depends(verify_token), id: str = Path(..., title="Position ID")
+    token: Payload = Depends(verify_token),
+    db: Database = Depends(get_db),
+    id: str = Path(..., title="Position ID"),
 ):
-    return crud_position.read_one(token["sub"], id)
+    return crud_position.read_one(db, token["sub"], id)
 
 
 @router.put("/@me/positions/{id}", response_model=PositionRead)
 def update_current_position(
     token: Payload = Depends(verify_token),
+    db: Database = Depends(get_db),
     id: str = Path(..., title="Position ID"),
     *,
-    position: PositionUpdate
+    position: PositionUpdate,
 ):
-    return crud_position.update(token["sub"], id, position)
+    return crud_position.update(db, token["sub"], id, position)
 
 
 @router.put("/@me/positions/{id}/picture", response_model=PositionRead)
 def update_current_picture(
     token: Payload = Depends(verify_token),
+    db: Database = Depends(get_db),
     id: str = Path(..., title="Position ID"),
     *,
-    picture: UploadFile
+    picture: UploadFile,
 ):
-    return crud_position.update_picture(token["sub"], id, picture)
+    return crud_position.update_picture(db, token["sub"], id, picture)
 
 
 @router.delete("/@me/positions/{id}", response_model=Message)
 def delete_current_position(
-    token: Payload = Depends(verify_token), id: str = Path(..., title="Position ID")
+    token: Payload = Depends(verify_token),
+    db: Database = Depends(get_db),
+    id: str = Path(..., title="Position ID"),
 ):
-    return crud_position.delete(token["sub"], id)
+    return crud_position.delete(db, token["sub"], id)
--- backend/app/app/crud/position.py
+++ backend/app/app/crud/position.py
@@ -1,70 +1,70 @@
 from fastapi import HTTPException, UploadFile, status
 from fastapi.encoders import jsonable_encoder
+from pymongo.database import Database
 
 from app.schemas.position import PositionCreate, PositionUpdate, Position
 from app.core.config import settings
-from app.db.client import client
 from app.utils.picture import update_picture
-
-db = client.resuilder
-col = db.positions
 
 
 class CRUDPosition:
-    def _get_by_user(self, user: str):
-        return list(col.find({"user": user}).limit(settings.CRUD_POSITIONS_LIMIT))
+    def _get_by_user(self, db: Database, user: str):
+        return list(
+            db.positions.find({"user": user}).limit(settings.CRUD_POSITIONS_LIMIT)
+        )
 
-    def _get_by_id(self, user: str, id: str):
-        doc = col.find_one({"user": user, "_id": id})
+    def _get_by_id(self, db: Database, user: str, id: str):
+        doc = db.positions.find_one({"user": user, "_id": id})
         if not doc:
             raise HTTPException(status.HTTP_404_NOT_FOUND)
         return doc
 
-    def _allow_new_doc(self, user: str):
+    def _allow_new_doc(self, db: Database, user: str):
         return (
             False
-            if col.count_documents({"user": user}) >= settings.CRUD_POSITIONS_LIMIT
+            if db.positions.count_documents({"user": user})
+            >= settings.CRUD_POSITIONS_LIMIT
             else True
         )
 
-    def create(self, user: str, position: PositionCreate):
-        if not self._allow_new_doc(user):
+    def create(self, db: Database, user: str, position: PositionCreate):
+        if not self._allow_new_doc(db, user):
             raise HTTPException(
                 status.HTTP_403_FORBIDDEN, "Maximum number of elements reached"
             )
         position_db = jsonable_encoder(Position.parse_obj(position))
-        id = col.insert_one({"user": user, **position_db}).inserted_id
-        return self._get_by_id(user, id)
+        id = db.positions.insert_one({"user": user, **position_db}).inserted_id
+        return self._get_by_id(db, user, id)
 
-    def read_one(self, user: str, id: str):
-        return self._get_by_id(user, id)
+    def read_one(self, db: Database, user: str, id: str):
+        return self._get_by_id(db, user, id)
 
-    def read_many(self, user: str):
-        return self._get_by_user(user)
+    def read_many(self, db: Database, user: str):
+        return self._get_by_user(db, user)
 
-    def update(self, user: str, id: str, position: PositionUpdate):
-        doc = self._get_by_id(user, id)
-        changes = col.update_one(
+    def update(self, db: Database, user: str, id: str, position: PositionUpdate):
+        doc = self._get_by_id(db, user, id)
+        changes = db.positions.update_one(
             {"user": user, "_id": doc["_id"]},
             {"$set": position.dict(exclude_none=True)},
         ).modified_count
-        return self._get_by_id(user, id) if changes else doc
+        return self._get_by_id(db, user, id) if changes else doc
 
-    def update_picture(self, user: str, id: str, picture: UploadFile):
-        doc = self._get_by_id(user, id)
+    def update_picture(self, db: Database, user: str, id: str, picture: UploadFile):
+        doc = self._get_by_id(db, user, id)
         res = update_picture(picture, doc["picture_id"])
         if res.get("error"):
             raise HTTPException(status.HTTP_400_BAD_REQUEST, res["error"])
         url = res["secure_url"]
-        changes = col.update_one(
+        changes = db.positions.update_one(
             {"user": user, "_id": doc["_id"]},
             {"$set": {"picture_url": url}},
         ).modified_count
-        return self._get_by_id(user, id) if changes else doc
+        return self._get_by_id(db, user, id) if changes else doc
 
-    def delete(self, user: str, id: str):
-        doc = self._get_by_id(user, id)
-        col.delete_one({"user": user, "_id": doc["_id"]})
+    def delete(self, db: Database, user: str, id: str):
+        doc = self._get_by_id(db, user, id)
+        db.positions.delete_one({"user": user, "_id": doc["_id"]})
         return {"msg": "ok"}
 
 
