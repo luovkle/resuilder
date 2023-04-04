0d911c4bfec9ee55ce49dc05b9c1f4c745192780
luovkle <roberto.silva.dev@protonmail.com>
2023-04-04 16:03:27 -0600

    ♻️ get_db dependency is implemented in endpoints /profiles

--- backend/app/app/api/api_v1/endpoints/profiles.py
+++ backend/app/app/api/api_v1/endpoints/profiles.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, UploadFile
+from pymongo.database import Database
 
-from app.api.deps import verify_token, get_access_token
+from app.api.deps import verify_token, get_access_token, get_db
 from app.schemas.payload import Payload
 from app.schemas.profile import ProfileRead, ProfileUpdate
 from app.crud.profile import crud_profile
@@ -12,25 +13,28 @@ router = APIRouter(prefix="/profiles", tags=["profiles"])
 def read_current_profile(
     token: Payload = Depends(verify_token),
     access_token: str = Depends(get_access_token),
+    db: Database = Depends(get_db),
 ):
-    return crud_profile.read(token["sub"], access_token)
+    return crud_profile.read(db, token["sub"], access_token)
 
 
 @router.put("/@me", response_model=ProfileRead)
 def update_current_profile(
     token: Payload = Depends(verify_token),
     access_token: str = Depends(get_access_token),
+    db: Database = Depends(get_db),
     *,
     profile: ProfileUpdate
 ):
-    return crud_profile.update(token["sub"], access_token, profile)
+    return crud_profile.update(db, token["sub"], access_token, profile)
 
 
 @router.put("/@me/picture", response_model=ProfileRead)
 def update_current_picture(
     token: Payload = Depends(verify_token),
     access_token: str = Depends(get_access_token),
+    db: Database = Depends(get_db),
     *,
     picture: UploadFile
 ):
-    return crud_profile.update_picture(token["sub"], access_token, picture)
+    return crud_profile.update_picture(db, token["sub"], access_token, picture)
--- backend/app/app/crud/profile.py
+++ backend/app/app/crud/profile.py
@@ -1,24 +1,21 @@
 from uuid import uuid4
 
 from fastapi import HTTPException, UploadFile, status
+from pymongo.database import Database
 
-from app.db.client import client
 from app.schemas.profile import ProfileUpdate
 from app.utils.profile import get_data
 from app.utils.picture import update_picture
 
-db = client.resuilder
-col = db.profiles
-
 
 class CRUDProfile:
-    def _get_by_user(self, user: str, access_token: str):
-        doc = col.find_one({"user": user})
+    def _get_by_user(self, db: Database, user: str, access_token: str):
+        doc = db.profiles.find_one({"user": user})
         if not doc:
             data = get_data(access_token)
             if not data:
                 raise HTTPException(status.HTTP_404_NOT_FOUND)
-            id = col.insert_one(
+            id = db.profiles.insert_one(
                 {
                     "user": user,
                     "name": data["name"],
@@ -27,33 +24,37 @@ class CRUDProfile:
                     "picture_id": uuid4().hex,
                 }
             ).inserted_id
-            doc = col.find_one({"_id": id})
+            doc = db.profiles.find_one({"_id": id})
         if not doc:
             raise HTTPException(status.HTTP_404_NOT_FOUND)
         return doc
 
-    def read(self, user: str, access_token: str):
-        return self._get_by_user(user, access_token)
+    def read(self, db: Database, user: str, access_token: str):
+        return self._get_by_user(db, user, access_token)
 
-    def update(self, user: str, access_token: str, profile: ProfileUpdate):
-        doc = self._get_by_user(user, access_token)
-        changes = col.update_one(
+    def update(
+        self, db: Database, user: str, access_token: str, profile: ProfileUpdate
+    ):
+        doc = self._get_by_user(db, user, access_token)
+        changes = db.profiles.update_one(
             {"_id": doc["_id"], "user": doc["user"]},
             {"$set": profile.dict(exclude_none=True)},
         ).modified_count
-        return self._get_by_user(user, access_token) if changes else doc
+        return self._get_by_user(db, user, access_token) if changes else doc
 
-    def update_picture(self, user: str, access_token: str, picture: UploadFile):
-        doc = self._get_by_user(user, access_token)
+    def update_picture(
+        self, db: Database, user: str, access_token: str, picture: UploadFile
+    ):
+        doc = self._get_by_user(db, user, access_token)
         res = update_picture(picture, doc["picture_id"])
         if res.get("error"):
             raise HTTPException(status.HTTP_400_BAD_REQUEST, res["error"])
         url = res["secure_url"]
-        changes = col.update_one(
+        changes = db.profiles.update_one(
             {"_id": doc["_id"], "user": doc["user"]},
             {"$set": {"picture_url": url}},
         ).modified_count
-        return self._get_by_user(user, access_token) if changes else doc
+        return self._get_by_user(db, user, access_token) if changes else doc
 
 
 crud_profile = CRUDProfile()
