66f45ed2b124965822ba2a665841d5b99e35bcd7
luovkle <roberto.silva.dev@protonmail.com>
2023-04-04 18:23:35 -0600

    ♻️ get_db dependency is implemented in endpoints /repositories

--- backend/app/app/api/api_v1/endpoints/repositories.py
+++ backend/app/app/api/api_v1/endpoints/repositories.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Path, Depends
+from pymongo.database import Database
 
-from app.api.deps import verify_token, get_access_token
+from app.api.deps import verify_token, get_access_token, get_db
 from app.schemas.payload import Payload
 from app.schemas.repository import RepositoryRead, RepositoryUpdate
 from app.crud.repository import crud_repository
@@ -12,22 +13,26 @@ router = APIRouter(prefix="/profiles", tags=["repositories"])
 def read_current_repositories(
     token: Payload = Depends(verify_token),
     access_token: str = Depends(get_access_token),
+    db: Database = Depends(get_db),
 ):
-    return crud_repository.read_many(token["sub"], access_token)
+    return crud_repository.read_many(db, token["sub"], access_token)
 
 
 @router.get("/@me/repositories/{id}", response_model=RepositoryRead)
 def read_current_repository(
-    token: Payload = Depends(verify_token), id: str = Path(..., title="Repository ID")
+    token: Payload = Depends(verify_token),
+    db: Database = Depends(get_db),
+    id: str = Path(..., title="Repository ID"),
 ):
-    return crud_repository.read_one(token["sub"], id)
+    return crud_repository.read_one(db, token["sub"], id)
 
 
 @router.put("/@me/repositories/{id}", response_model=RepositoryRead)
 def update_current_repository(
     token: Payload = Depends(verify_token),
+    db: Database = Depends(get_db),
     id: str = Path(..., title="Repository ID"),
     *,
     repository: RepositoryUpdate
 ):
-    return crud_repository.update(token["sub"], id, repository)
+    return crud_repository.update(db, token["sub"], id, repository)
--- backend/app/app/crud/repository.py
+++ backend/app/app/crud/repository.py
@@ -1,27 +1,28 @@
 from fastapi import HTTPException, status
 from fastapi.encoders import jsonable_encoder
+from pymongo.database import Database
 
-from app.db.client import client
 from app.schemas.repository import Repository, RepositoryUpdate
 from app.utils.profile import get_data
 from app.utils.repository import get_repositories
 from app.core.config import settings
 
-db = client.resuilder
-col = db.repositories
-
 
 class CRUDRepository:
-    def _create_repositories(self, user: str, repositories_data: list):
+    def _create_repositories(self, db: Database, user: str, repositories_data: list):
         repositories = []
         for repository in repositories_data:
             repository_db = jsonable_encoder(Repository(**repository))
             repositories.append({"user": user, **repository_db})
-        col.insert_many(repositories)
-        return list(col.find({"user": user}).limit(settings.CRUD_REPOSITORIES_LIMIT))
+        db.repositories.insert_many(repositories)
+        return list(
+            db.repositories.find({"user": user}).limit(settings.CRUD_REPOSITORIES_LIMIT)
+        )
 
-    def _get_by_user(self, user: str, access_token: str):
-        doc = list(col.find({"user": user}).limit(settings.CRUD_REPOSITORIES_LIMIT))
+    def _get_by_user(self, db: Database, user: str, access_token: str):
+        doc = list(
+            db.repositories.find({"user": user}).limit(settings.CRUD_REPOSITORIES_LIMIT)
+        )
         if not doc:
             data = get_data(access_token)
             if not data:
@@ -32,30 +33,30 @@ class CRUDRepository:
             )
             if not repositories_data:
                 return []
-            doc = self._create_repositories(user, repositories_data)
+            doc = self._create_repositories(db, user, repositories_data)
         if not doc:
             raise HTTPException(status.HTTP_404_NOT_FOUND)
         return doc
 
-    def _get_by_id(self, user: str, id: str):
-        doc = col.find_one({"user": user, "_id": id})
+    def _get_by_id(self, db: Database, user: str, id: str):
+        doc = db.repositories.find_one({"user": user, "_id": id})
         if not doc:
             raise HTTPException(status.HTTP_404_NOT_FOUND)
         return doc
 
-    def read_one(self, user: str, id: str):
-        return self._get_by_id(user, id)
+    def read_one(self, db: Database, user: str, id: str):
+        return self._get_by_id(db, user, id)
 
-    def read_many(self, user: str, access_token: str):
-        return self._get_by_user(user, access_token)
+    def read_many(self, db: Database, user: str, access_token: str):
+        return self._get_by_user(db, user, access_token)
 
-    def update(self, user: str, id: str, repository: RepositoryUpdate):
-        doc = self._get_by_id(user, id)
-        changes = col.update_one(
+    def update(self, db: Database, user: str, id: str, repository: RepositoryUpdate):
+        doc = self._get_by_id(db, user, id)
+        changes = db.repositories.update_one(
             {"_id": doc["_id"], "user": doc["user"]},
             {"$set": repository.dict(exclude_none=True)},
         ).modified_count
-        return self._get_by_id(user, id) if changes else doc
+        return self._get_by_id(db, user, id) if changes else doc
 
 
 crud_repository = CRUDRepository()
