3a2b2a5e27f0355c33f7695f6f21c2be3aa53352
luovkle <roberto.silva.dev@protonmail.com>
2023-04-15 17:43:07 -0600

    ✨ Add Skill models

---
+++ backend/app/app/schemas/skill.py
@@ -0,0 +1,23 @@
+from uuid import uuid4
+
+from pydantic import BaseModel, Field
+
+
+class SkillBase(BaseModel):
+    name: str
+
+
+class Skill(SkillBase):
+    id: str = Field(default_factory=uuid4, alias="_id")
+
+
+class SkillCreate(SkillBase):
+    ...
+
+
+class SkillRead(SkillBase):
+    id: str = Field(alias="_id")
+
+
+class SkillUpdate(BaseModel):
+    name: str | None = None
