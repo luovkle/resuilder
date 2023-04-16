ac95e442337ce7066d84529920261cc9d02329f6
luovkle <roberto.silva.dev@protonmail.com>
2023-04-15 18:34:02 -0600

    ✨ Replace existing CRUD operations for Skills with new ones

--- backend/app/app/api/api_v1/endpoints/skills.py
+++ backend/app/app/api/api_v1/endpoints/skills.py
@@ -1,26 +1,57 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Path
 from pymongo.database import Database
 
 from app.api.deps import verify_token, get_db
 from app.schemas.payload import Payload
-from app.schemas.skills import SkillsRead, SkillsUpdate
-from app.crud.skills import crud_skills
+from app.schemas.message import Message
+
+from app.schemas.skill import SkillCreate, SkillRead, SkillUpdate
+from app.crud.skill import crud_skill
 
 router = APIRouter(prefix="/profiles", tags=["skills"])
 
 
-@router.get("/@me/skills", response_model=SkillsRead)
-def read_current_skills(
-    token: Payload = Depends(verify_token), db: Database = Depends(get_db)
-):
-    return crud_skills.read(db, token["sub"])
-
-
-@router.put("/@me/skills", response_model=SkillsRead)
-def update_current_skills(
+@router.post("/@me/skills", response_model=SkillRead)
+def create_skill(
     token: Payload = Depends(verify_token),
     db: Database = Depends(get_db),
     *,
-    skills: SkillsUpdate
+    skill: SkillCreate
 ):
-    return crud_skills.update(db, token["sub"], skills)
+    return crud_skill.create(db, token["sub"], skill)
+
+
+@router.get("/@me/skills", response_model=list[SkillRead])
+def read_current_skills(
+    token: Payload = Depends(verify_token), db: Database = Depends(get_db)
+):
+    return crud_skill.read_many(db, token["sub"])
+
+
+@router.get("/@me/skills/{id}", response_model=SkillRead)
+def read_current_skill(
+    token: Payload = Depends(verify_token),
+    db: Database = Depends(get_db),
+    id: str = Path(..., title="Skill ID"),
+):
+    return crud_skill.read_one(db, token["sub"], id)
+
+
+@router.put("/@me/skills/{id}", response_model=SkillRead)
+def update_current_skills(
+    token: Payload = Depends(verify_token),
+    db: Database = Depends(get_db),
+    id: str = Path(..., title="Skill ID"),
+    *,
+    skill: SkillUpdate
+):
+    return crud_skill.update(db, token["sub"], id, skill)
+
+
+@router.delete("/@me/skills/{id}", response_model=Message)
+def delete_current_skill(
+    token: Payload = Depends(verify_token),
+    db: Database = Depends(get_db),
+    id: str = Path(..., title="Skill ID"),
+):
+    return crud_skill.delete(db, token["sub"], id)
