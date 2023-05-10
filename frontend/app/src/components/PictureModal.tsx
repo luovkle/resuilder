7163fce406c0b458c61d9f3ce589ba3cfc0c0655
luovkle <roberto.silva.dev@protonmail.com>
2023-05-09 18:18:29 -0600

    ♻️ Refactor useEffect hook in PictureModal component

--- frontend/app/src/components/PictureModal.tsx
+++ frontend/app/src/components/PictureModal.tsx
@@ -22,9 +22,14 @@ export const PictureModal = ({ showModal, url, alt, newPicture }: Props) => {
 
   useEffect(() => {
     document.addEventListener("click", handleClick, true);
-    document.addEventListener("keydown", handleKey);
     return () => {
       document.removeEventListener("click", handleClick, true);
+    };
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKey);
+    return () => {
       document.removeEventListener("keydown", handleKey);
     };
   }, []);
