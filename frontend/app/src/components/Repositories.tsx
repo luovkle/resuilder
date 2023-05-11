5389cb986d348ba9e08bd17cd21f87f1fb7ad16d
luovkle <roberto.silva.dev@protonmail.com>
2023-05-10 19:48:57 -0600

    💄 Fix overflow issue when loading Select component

--- frontend/app/src/components/Repositories.tsx
+++ frontend/app/src/components/Repositories.tsx
@@ -10,40 +10,42 @@ export const Repositories = () => {
 
   return (
     <Section name="Repositories" onMenu={() => setRenderOptions(true)}>
-      <div className="grid grid-cols-2 gap-4">
-        {repositories.map(
-          (repository) =>
-            repository.show && (
-              <div
-                key={repository._id}
-                className="col-span-1 space-y-3 p-5 border border-gray-700 rounded-lg"
-              >
-                <a
-                  href={repository.url}
-                  target="_blank"
-                  className="text-base font-semibold text-blue-400"
+      <div className="overflow-hidden">
+        <div className="grid grid-cols-2 gap-4">
+          {repositories.map(
+            (repository) =>
+              repository.show && (
+                <div
+                  key={repository._id}
+                  className="col-span-1 space-y-3 p-5 border border-gray-700 rounded-lg"
                 >
-                  {repository.name}
-                </a>
-                <p className="text-gray-400 truncate">
-                  {repository.description}
-                </p>
-                <div className="space-x-5 text-sm text-gray-300">
-                  <span className="font-semibold">{repository.lang}</span>
-                  <span>Stars: {repository.stars}</span>
-                  <span>Forks: {repository.forks}</span>
+                  <a
+                    href={repository.url}
+                    target="_blank"
+                    className="text-base font-semibold text-blue-400"
+                  >
+                    {repository.name}
+                  </a>
+                  <p className="text-gray-400 truncate">
+                    {repository.description}
+                  </p>
+                  <div className="space-x-5 text-sm text-gray-300">
+                    <span className="font-semibold">{repository.lang}</span>
+                    <span>Stars: {repository.stars}</span>
+                    <span>Forks: {repository.forks}</span>
+                  </div>
                 </div>
-              </div>
-            )
+              )
+          )}
+        </div>
+        {renderOptions && (
+          <Select
+            showMenu={setRenderOptions}
+            repositories={repositories}
+            updateRepository={updateRepository}
+          />
         )}
       </div>
-      {renderOptions && (
-        <Select
-          showMenu={setRenderOptions}
-          repositories={repositories}
-          updateRepository={updateRepository}
-        />
-      )}
     </Section>
   );
 };
