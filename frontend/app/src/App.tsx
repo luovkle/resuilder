e560e881c9d2f009bed13bd90385888e1308927c
luovkle <roberto.silva.dev@protonmail.com>
2023-05-10 23:12:35 -0600

    ✨ Implemented Navbar component

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -2,7 +2,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 
 import { AppPage, HomePage } from "./pages";
-import { LoginButton, LogoutButton, Spinner } from "./components";
+import { Spinner, Navbar } from "./components";
 
 export const App = () => {
   const { isAuthenticated, isLoading } = useAuth0();
@@ -13,7 +13,7 @@ export const App = () => {
 
   return (
     <>
-      {isAuthenticated ? <LogoutButton /> : <LoginButton />}
+      <Navbar />
       <Routes>
         <Route index element={isAuthenticated ? <AppPage /> : <HomePage />} />
         <Route path="*" element={<Navigate to="/" />} />
