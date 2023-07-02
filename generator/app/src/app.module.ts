9cec50badefba416427ce489d42934f178fc4e19
luovkle <roberto.silva.dev@protonmail.com>
2023-07-02 00:02:13 -0600

    🔧 Add MongoDB configuration for 'generator' service

--- generator/app/src/app.module.ts
+++ generator/app/src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ResumeModule } from './resume/resume.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [ResumeModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('uri'),
+      }),
+      inject: [ConfigService],
+    }),
+    ResumeModule,
+  ],
   controllers: [],
   providers: [],
 })
