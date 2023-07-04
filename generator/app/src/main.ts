a4c0d64c4e987b41b5ed9864c45440bc4f2bab5d
luovkle <roberto.silva.dev@protonmail.com>
2023-07-03 20:51:52 -0600

    🔧 Add nunjucks configuration

--- generator/app/src/main.ts
+++ generator/app/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { configure as nunjucksConfigure } from 'nunjucks';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  nunjucksConfigure('src/resume/static', { autoescape: true });
   await app.listen(8001);
 }
 bootstrap();
