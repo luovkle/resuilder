eca897f95ec2197d3a07e5ee31f58d45492a357f
luovkle <roberto.silva.dev@protonmail.com>
2023-07-05 17:46:46 -0600

    ✨ Add download button component

---
+++ frontend/app/src/components/DownloadButton.tsx
@@ -0,0 +1,34 @@
+import { useAuth0 } from "@auth0/auth0-react";
+
+export default function DownloadButton() {
+  const { getAccessTokenSilently } = useAuth0();
+
+  const handleClick = async () => {
+    const accessToken = await getAccessTokenSilently();
+    console.log(accessToken);
+    fetch("http://localhost:8000/profiles/@me/resumes", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/pdf",
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+      .then((res) => res.blob())
+      .then((blob) => {
+        const element = document.createElement("a");
+        element.href = URL.createObjectURL(blob);
+        element.download = "resume.pdf";
+        document.body.appendChild(element);
+        element.click();
+      });
+  };
+
+  return (
+    <button
+      className="bg-blue-600 hover:bg-blue-500 px-4 py-1"
+      onClick={handleClick}
+    >
+      Download
+    </button>
+  );
+}
--- frontend/app/src/components/Navbar.tsx
+++ frontend/app/src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import { useAuth0 } from "@auth0/auth0-react";
 
 import { LoginButton, LogoutButton, Title } from "./";
+import DownloadButton from "./DownloadButton";
 
 export const Navbar = () => {
   const { isAuthenticated } = useAuth0();
@@ -8,7 +9,14 @@ export const Navbar = () => {
   return (
     <nav className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-5">
       <Title />
-      {isAuthenticated ? <LogoutButton /> : <LoginButton />}
+      {isAuthenticated ? (
+        <div className="space-x-2.5">
+          <DownloadButton />
+          <LogoutButton />
+        </div>
+      ) : (
+        <LoginButton />
+      )}
     </nav>
   );
 };
