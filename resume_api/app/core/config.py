6942c397246fdfda573e3eb5f0b76af5e36e6da1
luovkle <roberto.silva.dev@protonmail.com>
2024-05-14 17:32:00 -0600

    ✨ Add settings

---
+++ resume_api/app/core/config.py
@@ -0,0 +1,27 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    # APP
+    APP_TITLE: str = "resuilder"
+    APP_VERSION: str = "0.2.0"
+    APP_DOCS_URL: str = "/"
+    APP_REDOC_URL: str | None = None
+    APP_ALLOW_ORIGINS: list[str] = []
+    APP_ALLOW_HEADERS: list[str] = ["Authorization"]
+    APP_ALLOW_METHODS: list[str] = ["OPTIONS", "GET", "POST", "PUT", "DELETE"]
+
+    # Auth0
+    AUTH0_DOMAIN: str
+    AUTH0_API_AUDIENCE: str
+    AUTH0_ISSUER: str
+    AUTH0_ALGORITHMS: str = "RS256"
+
+    # Cloudinary
+    CLOUDINARY_URL: str
+
+    # DB
+    DB_URI: str
+
+
+settings = Settings()  # type: ignore
