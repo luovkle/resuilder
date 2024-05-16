70db6820e1ea455d94c4fc58311b1257e76052cb
luovkle <roberto.silva.dev@protonmail.com>
2024-05-16 13:54:24 -0600

    ✨ Add profiles endpoints

--- resume_api/app/api/main.py
+++ resume_api/app/api/main.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter
 
 from app.api.routes import contact_methods
+from app.api.routes import profiles
 
 api_router = APIRouter()
 api_router.include_router(
     contact_methods.router, prefix="/contact-methods", tags=["contact-methods"]
+)
+api_router.include_router(
+    profiles.router, prefix="/profiles", tags=["profiles"]
 )
---
+++ resume_api/app/api/routes/profiles.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+
+from app.schemas.profile import ProfileUpdate, ProfileRead
+
+router = APIRouter()
+
+
+@router.get("/@me", response_model=ProfileRead)
+def read_profile_current_user(): ...
+
+
+@router.patch("/@me", response_model=ProfileRead)
+def update_profile_current_user(new_data: ProfileUpdate): ...
