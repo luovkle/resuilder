6e074b0c78786cf15694ffac7edd220af77141d0
luovkle <roberto.silva.dev@protonmail.com>
2024-05-16 14:22:04 -0600

    🎨 Format app/api/main file

--- resume_api/app/api/main.py
+++ resume_api/app/api/main.py
@@ -1,24 +1,35 @@
 from fastapi import APIRouter
 
-from app.api.routes import contact_methods
-from app.api.routes import profiles
-from app.api.routes import jobs
-from app.api.routes import projects
-from app.api.routes import resumes
+from app.api.routes import contact_methods, profiles, jobs, projects, resumes
 
 api_router = APIRouter()
+
 api_router.include_router(
-    resumes.router, prefix="/resumes", tags=["resumes"]
+    resumes.router,
+    prefix="/resumes",
+    tags=["resumes"],
 )
+
 api_router.include_router(
-    contact_methods.router, prefix="/contact-methods", tags=["contact-methods"]
+    contact_methods.router,
+    prefix="/contact-methods",
+    tags=["contact-methods"],
 )
+
 api_router.include_router(
-    profiles.router, prefix="/profiles", tags=["profiles"]
+    profiles.router,
+    prefix="/profiles",
+    tags=["profiles"],
 )
+
 api_router.include_router(
-    jobs.router, prefix="/jobs", tags=["jobs"]
+    jobs.router,
+    prefix="/jobs",
+    tags=["jobs"],
 )
+
 api_router.include_router(
-    projects.router, prefix="/projects", tags=["projects"]
+    projects.router,
+    prefix="/projects",
+    tags=["projects"],
 )
