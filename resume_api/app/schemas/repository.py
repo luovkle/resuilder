10ea6b36291b5add0819db35caae97c5ddf7c144
luovkle <roberto.silva.dev@protonmail.com>
2024-05-15 19:55:42 -0600

    ✨ Add repository models

---
+++ resume_api/app/schemas/repository.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, HttpUrl, field_serializer
+
+
+class Lang(BaseModel):
+    color: str | None = None
+    name: str
+    percentage: float | None = None
+
+
+class Repository(BaseModel):
+    name: str
+    url: HttpUrl
+    description: str
+    langs: list[Lang] = []
+
+    @field_serializer("url")
+    def serialize_dt(self, value: HttpUrl):
+        return str(value)
