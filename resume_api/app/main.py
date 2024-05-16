926448999bcf8e796d70e4dd8ff22f64ebc26ebb
luovkle <roberto.silva.dev@protonmail.com>
2024-05-16 15:27:15 -0600

    ✨ Add config

--- resume_api/app/main.py
+++ resume_api/app/main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.main import api_router
+from app.core.config import settings
 
-app = FastAPI()
+app = FastAPI(
+    title=settings.APP_TITLE,
+    version=settings.APP_VERSION,
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.CORS_ALLOW_ORIGINS,
+    allow_methods=settings.CORS_ALLOW_METHODS,
+    allow_headers=settings.CORS_ALLOW_HEADERS,
+    allow_credentials=settings.CORS_ALLOW_CREDENTIALS,
+)
+
 app.include_router(api_router)
