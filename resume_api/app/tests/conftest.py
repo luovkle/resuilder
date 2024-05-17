3dd3f10feea301fc032669373f0aea24eec663cb
luovkle <roberto.silva.dev@protonmail.com>
2024-05-17 15:58:23 -0600

    ✨ Add pytest configs

---
+++ resume_api/app/tests/conftest.py
@@ -0,0 +1,25 @@
+import pytest
+from pymongo import MongoClient
+from pymongo.database import Database
+from fastapi.testclient import TestClient
+
+from app.main import app
+from app.api.deps import get_db
+from app.tests.config import test_settings
+
+
+@pytest.fixture(name="db")
+def db_fixture():
+    client = MongoClient(test_settings.TEST_DB_URI)
+    db = client[test_settings.TEST_DB_NAME]
+    yield db
+    client.drop_database(db)
+    client.close()
+
+
+@pytest.fixture(name="client")
+def client_fixture(db: Database):
+    app.dependency_overrides[get_db] = lambda: db
+    client = TestClient(app)
+    yield client
+    app.dependency_overrides.clear()
