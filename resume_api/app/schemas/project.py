0941e94a4c595e5b42e1680a373d7eb7257f3388
luovkle <roberto.silva.dev@protonmail.com>
2024-05-18 17:20:54 -0600

    🐛 Fix projects models

--- resume_api/app/schemas/project.py
+++ resume_api/app/schemas/project.py
@@ -4,8 +4,9 @@ from pydantic import BaseModel, Field, HttpUrl, field_serializer
 
 
 class Lang(BaseModel):
+    color: str | None = None
     name: str
-    percentage: float
+    percentage: float | None = None
 
 
 class ProjectBase(BaseModel):
@@ -19,6 +20,10 @@ class ProjectBase(BaseModel):
 class ProjectDB(ProjectBase):
     id: UUID = Field(default_factory=uuid4, alias="_id")
     user_id: str
+
+    @field_serializer("id")
+    def serialize_id(self, id: UUID):
+        return str(id)
 
     @field_serializer("url")
     def serialize_url(self, url: HttpUrl):
