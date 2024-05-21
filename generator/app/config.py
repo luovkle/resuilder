3012570fa6ec98278a37321bd555c5bc3d5f7afd
luovkle <roberto.silva.dev@protonmail.com>
2024-05-20 21:27:00 -0600

    ✨ Update config

--- generator/app/config.py
+++ generator/app/config.py
@@ -1,9 +1,13 @@
-from pydantic import WebsocketUrl
+from pydantic import HttpUrl, WebsocketUrl
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+    # Browser
     BROWSER_WS_URL: WebsocketUrl
+
+    # Renderer
+    RENDERER_HTTP_URL: HttpUrl
 
 
 settings = Settings()  # type: ignore[call-arg]
