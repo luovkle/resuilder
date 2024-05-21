5b46550bae59eb94603504ea48deeca8dbb98587
luovkle <roberto.silva.dev@protonmail.com>
2024-05-21 14:56:49 -0600

    🚚 Update routes

--- generator/app/routes.py
+++ generator/app/routes.py
@@ -3,9 +3,9 @@ from fastapi import APIRouter
 from app.schemas import Url
 from app.service import generate_resume
 
-router = APIRouter(prefix="/generator")
+router = APIRouter(prefix="/pdf", tags=["PDF"])
 
 
-@router.post("/pdf/{user}", response_model=Url)
+@router.post("/{user}", response_model=Url)
 async def generate_resume_pdf(user: str):
     return await generate_resume(user)
