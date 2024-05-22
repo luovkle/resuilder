8248aafcb4be6fc151838f80dbcb557ccb370f06
luovkle <roberto.silva.dev@protonmail.com>
2024-05-22 14:21:08 -0600

    ✨ Add queryClient

--- editor/src/main.tsx
+++ editor/src/main.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./services/queries";
 import App from "./App.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
