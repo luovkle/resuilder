50024388082aca522baa1ccc77dd4d0ddbfcee03
luovkle <roberto.silva.dev@protonmail.com>
2024-05-22 14:23:14 -0600

    ✨ Add react query devtools

--- editor/src/main.tsx
+++ editor/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryClient } from "./services/queries";
 import App from "./App.tsx";
 import "./index.css";
@@ -9,6 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>,
 );
