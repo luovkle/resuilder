27c5757b89158418c7a0d55bbf071c0bc51f8467
luovkle <roberto.silva.dev@protonmail.com>
2024-05-23 14:58:47 -0600

    ✨ Add useAuth hook

---
+++ editor/src/hooks/useAuth.ts
@@ -0,0 +1,30 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import { useState, useEffect } from "react";
+
+const useAuth = () => {
+  const { getAccessTokenSilently, isAuthenticated, loginWithRedirect } =
+    useAuth0();
+  const [accessToken, setAccessToken] = useState<string | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchAccessToken = async () => {
+      try {
+        if (isAuthenticated) {
+          const token = await getAccessTokenSilently();
+          setAccessToken(token);
+        } else {
+          loginWithRedirect();
+        }
+      } catch (err: any) {
+        setError(err.message);
+      }
+    };
+
+    fetchAccessToken();
+  }, [getAccessTokenSilently, isAuthenticated, loginWithRedirect]);
+
+  return { accessToken, error };
+};
+
+export default useAuth;
