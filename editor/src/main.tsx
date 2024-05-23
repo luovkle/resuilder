bcb010ac3996d24f8832bd700bf58db78ebd3e65
luovkle <roberto.silva.dev@protonmail.com>
2024-05-23 12:40:16 -0600

    ✨ Add Auth0ProviderWithRedirectCallback wrapper

--- editor/src/main.tsx
+++ editor/src/main.tsx
@@ -4,6 +4,7 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryClient } from "./services/queries";
 import { BrowserRouter } from "react-router-dom";
+import { Auth0ProviderWithRedirectCallback } from "./components";
 import App from "./App.tsx";
 import "./index.css";
 
@@ -11,8 +12,10 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <App />
-        <ReactQueryDevtools initialIsOpen={false} />
+        <Auth0ProviderWithRedirectCallback>
+          <App />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </Auth0ProviderWithRedirectCallback>
       </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>,
