0b9d6d5d419fd67ea9d7d2ffbeee48e9e26ded29
luovkle <roberto.silva.dev@protonmail.com>
2024-05-23 12:54:32 -0600

    ✨ Add auth components

--- editor/src/App.tsx
+++ editor/src/App.tsx
@@ -1,6 +1,17 @@
-import { Navbar, Resume } from "./components";
+import { useAuth0 } from "@auth0/auth0-react";
+import { LoginButton, Navbar, Resume } from "./components";
 
 const App = () => {
+  const { isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
+  if (!isAuthenticated) {
+    return <LoginButton />;
+  }
+
   return (
     <>
       <Navbar />
