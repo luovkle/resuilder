4adc4f7e1a2ebad23a3f5ed0cbf1c0b30775d009
luovkle <roberto.silva.dev@protonmail.com>
2024-05-24 17:52:45 -0600

    ✨ Add useResume hook

---
+++ editor/src/hooks/useResume.ts
@@ -0,0 +1,45 @@
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import type { ResumeUpdate } from "../services/api";
+import useResumeApi from "./useResumeApi";
+
+const useResume = () => {
+  const queryClient = useQueryClient();
+  const { createResume, readResume, updateResume, deleteResume } =
+    useResumeApi();
+
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["resume"],
+    queryFn: readResume,
+  });
+
+  const createResumeMutation = useMutation({
+    mutationFn: createResume,
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["resume"] }),
+  });
+
+  const updateResumeMutation = useMutation({
+    mutationFn: updateResume,
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["resume"] }),
+  });
+
+  const deleteResumeMutation = useMutation({
+    mutationFn: deleteResume,
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["resume"] }),
+  });
+
+  return {
+    resume: data,
+    isLoading,
+    isError,
+    error,
+    createResume: () => createResumeMutation.mutate(),
+    updateResume: (data: ResumeUpdate) => {
+      updateResumeMutation.mutate(data);
+    },
+    deleteResume: () => {
+      deleteResumeMutation.mutate();
+    },
+  };
+};
+
+export default useResume;
