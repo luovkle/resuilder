4f265d566f6e6283aeb315445400d73f82da1460
luovkle <roberto.silva.dev@protonmail.com>
2024-05-27 17:33:25 -0600

    🔥 Remove Tag component

--- editor/src/components/experience/index.tsx
+++ editor/src/components/experience/index.tsx
@@ -2,4 +2,3 @@ export { default as DefaultPicture } from "./DefaultPicture";
 export { default as Responsibilities } from "./Responsibilities";
 export { default as TechStack } from "./TechStack";
 export { default as Tools } from "./Tools";
-export { default as Tag } from "./Tag";
