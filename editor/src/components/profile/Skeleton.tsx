8ee4f1358c484406b83b3db429abe87bb49efa74
luovkle <roberto.silva.dev@protonmail.com>
2024-05-26 19:34:47 -0600

    ✨ Update Skeleton component for Profile

--- editor/src/components/profile/Skeleton.tsx
+++ editor/src/components/profile/Skeleton.tsx
@@ -1,9 +1,8 @@
 const Skeleton = () => {
   return (
-    <div role="status" className="max-w-sm animate-pulse space-y-2">
-      <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
-      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px]"></div>
-      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px]"></div>
+    <div role="status" className="max-w-sm animate-pulse space-y-8">
+      <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-40 mb-2.5"></div>
+      <div className="h-2.5 mx-auto bg-gray-300 rounded-full dark:bg-gray-700 max-w-[540px]"></div>
     </div>
   );
 };
