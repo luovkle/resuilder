1b09bb31c521b382e9c56b42c87023f66a887d5e
luovkle <roberto.silva.dev@protonmail.com>
2024-05-28 16:58:54 -0600

    🔨 Update scripts

--- editor/scripts/generate-clients.sh
+++ editor/scripts/generate-clients.sh
@@ -8,3 +8,8 @@ pnpm openapi-generator-cli generate \
   -i $VITE_EDITOR_API_HTTP_URL/openapi.json \
   -g typescript-axios \
   -o ./src/services/api/
+
+pnpm openapi-generator-cli generate \
+  -i $VITE_GENERATOR_HTTP_URL/openapi.json \
+  -g typescript-axios \
+  -o ./src/services/generatorApi/
