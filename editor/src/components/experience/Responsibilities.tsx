1a2eab4a8f93b6c257617bdd198d8367bdeb3166
luovkle <roberto.silva.dev@protonmail.com>
2024-05-28 15:28:07 -0600

    ✨ Implement Text components in the Responsibilities component

--- editor/src/components/experience/Responsibilities.tsx
+++ editor/src/components/experience/Responsibilities.tsx
@@ -1,10 +1,36 @@
 import type { JobRead } from "../../services/api";
+import NewText from "./NewText";
+import Text from "./Text";
 
 interface Props {
   job: JobRead;
+  updateResponsibilities: (id: string, responsibilities: string[]) => void;
 }
 
-const Responsibilities = ({ job }: Props) => {
+const Responsibilities = ({ job, updateResponsibilities }: Props) => {
+  const addResponsibility = (responsibility: string) => {
+    updateResponsibilities(job.id, [
+      ...(job.responsibilities || []),
+      responsibility,
+    ]);
+  };
+
+  const updateResponsibility = (idx: number, responsibility: string) => {
+    updateResponsibilities(
+      job.id,
+      job.responsibilities?.map((cResponsibility, cIdx) =>
+        cIdx === idx ? responsibility : cResponsibility,
+      ) || [],
+    );
+  };
+
+  const deleteResponsibility = (idx: number) => {
+    updateResponsibilities(
+      job.id,
+      job.responsibilities?.filter((_, cIdx) => cIdx !== idx) || [],
+    );
+  };
+
   return (
     <div className="py-2.5 space-y-2.5">
       <div>
@@ -12,11 +38,29 @@ const Responsibilities = ({ job }: Props) => {
           Responsibilities
         </h5>
       </div>
-      <ul className="pl-10 list-disc">
-        {job.responsibilities?.map((responsibility) => (
-          <li key={responsibility}>{responsibility}</li>
-        ))}
-      </ul>
+      {job.responsibilities && job.responsibilities?.length > 0 ? (
+        <ul className="pl-10 list-disc">
+          {job.responsibilities?.map((responsibility, idx) => (
+            <li key={responsibility}>
+              <Text
+                idx={idx}
+                text={responsibility}
+                updateText={updateResponsibility}
+                deleteText={deleteResponsibility}
+              />
+            </li>
+          ))}
+          <li>
+            <div className="mt-2">
+              <NewText addText={addResponsibility} />
+            </div>
+          </li>
+        </ul>
+      ) : (
+        <div className="mt-2">
+          <NewText addText={addResponsibility} />
+        </div>
+      )}
     </div>
   );
 };
