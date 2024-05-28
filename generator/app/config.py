edc6a0ea68d29903e7b7ddc9bc56fc47256f1482
luovkle <roberto.silva.dev@protonmail.com>
2024-05-28 17:40:40 -0600

    ✨ Add CORS config

--- generator/app/config.py
+++ generator/app/config.py
@@ -3,6 +3,16 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+    # App
+    APP_TITLE: str = "generator"
+    APP_VERSION: str = "0.2.0"
+
+    # CORS
+    CORS_ALLOW_ORIGINS: list[str] = []
+    CORS_ALLOW_HEADERS: list[str] = ["Authorization"]
+    CORS_ALLOW_METHODS: list[str] = ["OPTIONS", "GET", "POST", "PUT", "DELETE"]
+    CORS_ALLOW_CREDENTIALS: bool = True
+
     # Browser
     BROWSER_WS_URL: WebsocketUrl
 
--- generator/app/main.py
+++ generator/app/main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+from app.config import settings
 from app.routes import router
 
-app = FastAPI()
+app = FastAPI(
+    title=settings.APP_TITLE,
+    version=settings.APP_VERSION,
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.CORS_ALLOW_ORIGINS,
+    allow_methods=settings.CORS_ALLOW_METHODS,
+    allow_headers=settings.CORS_ALLOW_HEADERS,
+    allow_credentials=settings.CORS_ALLOW_CREDENTIALS,
+)
+
 app.include_router(router)
