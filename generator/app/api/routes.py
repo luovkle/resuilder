0d0345474cf4fac3b3b1a63505173238b99fb8de
luovkle <roberto.silva.dev@protonmail.com>
2024-07-12 20:00:10 -0600

    🔒️ Add authentication to the generator service endpoints

--- generator/app/api/routes.py
+++ generator/app/api/routes.py
@@ -1,5 +1,8 @@
-from fastapi import APIRouter
+from typing import Annotated
 
+from fastapi import APIRouter, Depends
+
+from app.api.deps import get_current_user
 from app.schemas import Url
 from app.service import generate_resume
 
@@ -7,5 +10,8 @@ router = APIRouter(prefix="/pdf", tags=["PDF"])
 
 
 @router.post("/{user}", response_model=Url)
-async def generate_resume_pdf(user: str):
+async def generate_resume_pdf(
+    _: Annotated[str, Depends(get_current_user)],
+    user: str,
+):
     return await generate_resume(user)
