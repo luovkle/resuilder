c98d9a3f01a3688266105f8f472d0df4eaf11a42
luovkle <roberto.silva.dev@protonmail.com>
2024-07-12 19:50:23 -0600

    🚚 Move routes.py file to api/routes.py

--- generator/app/main.py
+++ generator/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.core.config import settings
-from app.routes import router
+from app.api.routes import router
 
 app = FastAPI(
     title=settings.APP_TITLE,
